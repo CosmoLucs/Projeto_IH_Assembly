1431dcb9e9a925e961a81f89c680b026e2667b52
Ju-ASilva <127569076+Ju-ASilva@users.noreply.github.com>
2024-06-13 22:36:44 -0300

    Update Q5.asm

--- Ficha02/Q5/Q5.asm
+++ Ficha02/Q5/Q5.asm
@@ -1,4 +1,4 @@
-lb x11, 0(x0)
+    lb x11, 0(x0)
     sb x11, 1030(x0)
     lb x5, 0(x0)
     lb x6, 0(x0)
@@ -7,12 +7,12 @@ lb x11, 0(x0)
     lb x29, 0(x0)
     lb x30, 0(x0)
     lb x31, 0(x0)
-    lb x9, 0(x0)
-    lb x18, 0(x0)
-    lb x19, 0(x0)
-    lb x20, 0(x0)
-    lb x21, 0(x0)
-    lb x22, 0(x0)
+	lb x9, 0(x0)
+	lb x18, 0(x0)
+	lb x19, 0(x0)
+	lb x20, 0(x0)
+	lb x21, 0(x0)
+	lb x22, 0(x0)
 
     addi x5, x5, 327
     addi x6, x6, 429
@@ -20,12 +20,12 @@ lb x11, 0(x0)
     addi x28, x28, 634
     addi x29, x29, 736
 
-    addi x9, x9, 1
-    addi x18, x18, 2
-    addi x19, x19, 4
-    addi x20, x20, 8
-    addi x21, x21, 16
-    addi x22, x22, 32
+	addi x9, x9, 1
+	addi x18, x18, 2
+	addi x19, x19, 4
+	addi x20, x20, 8
+	addi x21, x21, 16
+	addi x22, x22, 32
 
 loop:
     lh x11, 1031(x0)
@@ -39,24 +39,27 @@ loop:
 
 yl:
     sb x9, 1027(x0)
-    beq x0, x0, loop
+    beq x0, x0, fim
 
 gl:
     sb x18, 1027(x0)
-    beq x0, x0, loop
+    beq x0, x0, fim
 
 gr:
     sb x19, 1027(x0)
-    beq x0, x0, loop
+    beq x0, x0, fim
 
 yr:
     sb x20, 1027(x0)
-    beq x0, x0, loop
+    beq x0, x0, fim
 
 rr:
     sb x21, 1027(x0)
-    beq x0, x0, loop
+    beq x0, x0, fim
 
 rl:
     sb x22, 1027(x0)
-    beq x0, x0, loop
+    beq x0, x0, fim
+
+fim:
+    halt
