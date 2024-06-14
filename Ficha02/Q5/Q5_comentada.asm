ad67437371436ad666c7a2da41b50450d1c89e30
Ju-ASilva <127569076+Ju-ASilva@users.noreply.github.com>
2024-06-13 22:39:21 -0300

    Update Q5_comentada.asm

--- Ficha02/Q5/Q5_comentada.asm
+++ Ficha02/Q5/Q5_comentada.asm
@@ -18,12 +18,12 @@
   lb x29, 0(x0)
   lb x30, 0(x0)
   lb x31, 0(x0)
-	lb x9, 0(x0)
-	lb x18, 0(x0)
-	lb x19, 0(x0)
-	lb x20, 0(x0)
-	lb x21, 0(x0)
-	lb x22, 0(x0)
+  lb x9, 0(x0)
+  lb x18, 0(x0)
+  lb x19, 0(x0)
+  lb x20, 0(x0)
+  lb x21, 0(x0)
+  lb x22, 0(x0)
     
   addi x5, x5, 327 // 327 representa 16 no sensor de temperatura
   addi x6, x6, 429 //429 representa 21 no sensor de temperatura
@@ -32,42 +32,44 @@
   addi x29, x29, 736//736 representa 36 no sensor de temperatura
 
   #numeros para ligar somente uma led
-	addi x9, x9, 1 #led amarela esquerda
-	addi x18, x18, 2 #led verde esquerda
-	addi x19, x19, 4 #led verde direita
-	addi x20, x20, 8 #led amarela direita
-	addi x21, x21, 16 # led vermelha direita
-	addi x22, x22, 32 #led vermelha esquerda
+  addi x9, x9, 1 #led amarela esquerda
+  addi x18, x18, 2 #led verde esquerda
+  addi x19, x19, 4 #led verde direita
+  addi x20, x20, 8 #led amarela direita
+  addi x21, x21, 16 # led vermelha direita
+  addi x22, x22, 32 #led vermelha esquerda
 
-loop:
-    lh x11, 1031(x0) #ler numero do sensor
+
+  lh x11, 1031(x0) #ler numero do sensor
  
-    blt x11, x5, yl #se for menor que 16 vai para yl (yellow left)
-    blt x11, x6, gl #se for menor que 21 vai para gl (green light)
-    blt x11, x7, gr #se for menor que 26 vai para gr (green right)
-    blt x11, x28, yr #se for menor que 31 vai para yr (yellow right)
-    blt x11, x29, rr #se for menor que 36 vai para rr (red right)
-    bge x11, x29, rl #se for maior ou igual que 36 vai para rl (red left)
+  blt x11, x5, yl #se for menor que 16 vai para yl (yellow left)
+  blt x11, x6, gl #se for menor que 21 vai para gl (green light)
+  blt x11, x7, gr #se for menor que 26 vai para gr (green right)
+  blt x11, x28, yr #se for menor que 31 vai para yr (yellow right)
+  blt x11, x29, rr #se for menor que 36 vai para rr (red right)
+  bge x11, x29, rl #se for maior ou igual que 36 vai para rl (red left)
 
 yl:
     sb x9, 1027(x0) #acender a led amarela esquerda
-    beq x0, x0, loop #voltar para o loop de ler o sensor
+    beq x0, x0, fim 
 
 gl:
     sb x18, 1027(x0) #acender a led verde esquerda
-    beq x0, x0, loop #voltar para o loop de ler o sensor
+    beq x0, x0, fim
 gr:
     sb x19, 1027(x0) #acender a led verde direita
-    beq x0, x0, loop #voltar para o loop de ler o sensor
+    beq x0, x0, fim
 
 yr:
     sb x20, 1027(x0) #acender a led amarela direita
-    beq x0, x0, loop #voltar para o loop de ler o sensor
+    beq x0, x0, fim
 
 rr:
     sb x21, 1027(x0) #acender a led vermelha direita
-    beq x0, x0, loop #voltar para o loop de ler o sensor
+    beq x0, x0, fim
 
 rl:
     sb x22, 1027(x0) ##acender a led vermelha esquerda
-    beq x0, x0, loop #voltar para o loop de ler o sensor
+    beq x0, x0, fim
+fim:
+    halt
